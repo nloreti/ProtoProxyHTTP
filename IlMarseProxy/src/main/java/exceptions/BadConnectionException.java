ef86bdc885bedf8afc4d153a2ce004c4680599c8
agustinmarseillan <agustinmarseillan@gmail.com>
2012-05-08 09:25:12 -0300

    exceptions

---
+++ IlMarseProxy/src/main/java/exceptions/BadConnectionException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class BadConnectionException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ IlMarseProxy/src/main/java/exceptions/BadResponseException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+public class BadResponseException extends Exception {
+
+	public BadResponseException() {
+	}
+	
+	public BadResponseException(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
