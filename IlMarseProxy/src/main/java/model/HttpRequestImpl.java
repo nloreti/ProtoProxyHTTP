eb4c08cbc9d47ba271402c5be67529ded7b57de7
Nloreti <@nloreti@gmail.com>
2012-06-02 11:29:49 -0300

    Saco Test de Maxi que ya no usamos

--- IlMarseProxy/src/main/java/model/HttpRequestImpl.java
+++ IlMarseProxy/src/main/java/model/HttpRequestImpl.java
@@ -124,67 +124,6 @@ public class HttpRequestImpl extends HttpMsg {
 
 	}
 
-	// ONLY FOR TESTING AND EXAMPLE
-	public static void test() throws ServerException, EncodingException {
-		final String aux = "GET /javase/1.4.2/docs/api/resources/inherit.gif HTTP/1.1\r\nHost: docs.oracle.com\r\nConnection: keep-alive\r\n"
-				+ "		Cache-Control: max-age=0\r\n"
-				+ "		If-Modified-Since: Wed, 07 Sep 2011 12:58:37 GMT\r\n"
-				+ "		User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19\r\n"
-				+ "		If-None-Match: \"220f4eda0bd49915699315f18b8b03cf:1326411148\"\r\n"
-				+ "		Accept: */*\r\n"
-				+ "		Referer: http://docs.oracle.com/javase/1.4.2/docs/api/java/util/regex/Pattern.html\r\n"
-				+ "		Accept-Encoding: gzip,deflate,sdch\r\n"
-				+ "		Accept-Language: en-US,en;q=0.8\r\n"
-				+ "		Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n"
-				+ "		Cookie: s_nr=1336338075458; tutorial_showLeftBar=yes; s_cc=true; s_sq=%5B%5BB%5D%5D";
-		final InputStream a = new InputStream() {
-			int i = 0;
-
-			// String a = aux;
-
-			@Override
-			public int read() throws IOException {
-				if (this.i < aux.length()) {
-					return aux.charAt(this.i++);
-				}
-				return -1;
-			}
-		};
-		HttpRequestImpl req = null;
-		try {
-			req = new HttpRequestImpl(a);
-			// System.out.println(req.getRequestURI() + req.getMethod() +
-			// req.getVersion());
-			// System.out.println(req.getHeaders());
-			System.out.println(req);
-		} catch (final ResponseException e) {
-			e.printStackTrace();
-		}
-		final String aux2 = req.toString();
-		final InputStream b = new InputStream() {
-			int i = 0;
-
-			// String a = aux2;
-
-			@Override
-			public int read() throws IOException {
-				if (this.i < aux2.length()) {
-					return aux2.charAt(this.i++);
-				}
-				return -1;
-			}
-		};
-		try {
-			req = new HttpRequestImpl(b);
-			// System.out.println(req.getRequestURI() + req.getMethod() +
-			// req.getVersion());
-			// System.out.println(req.getHeaders());
-			System.out.println(req);
-		} catch (final ResponseException e) {
-			e.printStackTrace();
-		}
-	}
-
 	@Override
 	public void writeStream(final OutputStream out) throws ServerException {
 		byte[] bytes;
